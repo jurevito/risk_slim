75534e951a3aa694921bb1bbff47f0acd451ac06
jurevito <jure.vito@gmail.com>
2020-10-04 18:39:45 +0200

    Implement scikit interface

--- dipnal.py
+++ dipnal.py
@@ -9,8 +9,13 @@ from riskslim.lattice_cpa import run_lattice_cpa
 
 # my own methods
 from diptools import build_model, split_data
-from riskmodel import RiskModel as rm
+from riskmodel import RiskModel
+import pandas as pd
 
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, roc_curve, auc
+"""
 # data
 os.chdir('..')
 data_name = "breastcancer"                                    # name of the data
@@ -84,11 +89,65 @@ print("--------PRINT MODEL INFO WITH KEY RESULTS--------")
 rm = build_model(model_info['solution'], data)
 #print(rm.predict_single([3,7,7,4,4,9,4,8,1]))
 
-mymatrix = np.matrix([[1,2,3,4],
-                      [5,6,7,8],
-                      [9,10,11,12]])
-
 print_model(model_info['solution'], data)
 rm.test_model(data)
-rm.test_model(test_data)
+rm.test_model(test_data) """
 
+# variables
+testSize = 0.2
+os.chdir('..')
+path = os.getcwd() + '/risk-slim/examples/data/' + 'mammo_data.csv'
+
+
+# read and prepocess data
+df_in  = pd.read_csv(path, float_precision='round_trip')
+X = df_in.iloc[:, :-1].values
+y = df_in.iloc[:,0].values
+X, y = shuffle(X, y, random_state=1)
+
+# split data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=testSize, random_state=0)
+
+params = {
+    'max_coefficient' : 5,
+    'max_L0_value' : 5,
+    'max_offset' : 50,
+    'c0_value' : 1e-6,
+    'w_pos' : 1.00
+}
+
+settings = {
+
+    'c0_value': params['c0_value'],
+    'w_pos': params['w_pos'],
+    #
+    # LCPA Settings
+    'max_runtime': 30.0,                               # max runtime for LCPA
+    'max_tolerance': np.finfo('float').eps,             # tolerance to stop LCPA (set to 0 to return provably optimal solution)
+    'display_cplex_progress': True,                     # print CPLEX progress on screen
+    'loss_computation': 'fast',                         # how to compute the loss function ('normal','fast','lookup')
+    #
+    # LCPA Improvements
+    'round_flag': True,                                # round continuous solutions with SeqRd
+    'polish_flag': True,                               # polish integer feasible solutions with DCD
+    'chained_updates_flag': True,                      # use chained updates
+    'add_cuts_at_heuristic_solutions': True,            # add cuts at integer feasible solutions found using polishing/rounding
+    #
+    # Initialization
+    'initialization_flag': True,                       # use initialization procedure
+    'init_max_runtime': 120.0,                         # max time to run CPA in initialization procedure
+    'init_max_coefficient_gap': 0.49,
+    #
+    # CPLEX Solver Parameters
+    'cplex_randomseed': 0,                              # random seed
+    'cplex_mipemphasis': 0,                             # cplex MIP strategy
+}
+
+
+rm = RiskModel(data_headers=df_in.columns.values, params=params, settings=settings)
+rm.fit_transform(X_train,y_train)
+y_pred = rm.predict(X_test)
+
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+print(accuracy_score(y_test, y_pred))
--- riskmodel.py
+++ riskmodel.py
@@ -1,51 +1,199 @@
+"""
+This is a module to be used as a reference for building other modules
+"""
 import numpy as np
 import math
+from sklearn.base import BaseEstimator, ClassifierMixin, TransformerMixin
+from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
+from sklearn.utils.multiclass import unique_labels
+from sklearn.metrics import euclidean_distances
 
-class RiskModel:
-    def __init__(self, rho_values, rho_names, intercept_val, selected_ind, filter_mask):
+from riskslim.helper_functions import load_data_from_csv, check_data, print_model
+from riskslim.setup_functions import get_conservative_offset
+from riskslim.coefficient_set import CoefficientSet
+from riskslim.lattice_cpa import run_lattice_cpa
 
-    	self.rho_values = rho_values
-    	self.rho_names = rho_names
-    	self.intercept_val = intercept_val
-    	self.selected_ind = selected_ind
-    	self.filter_mask = filter_mask
-    	self.threshold = 0.5
 
-    def predict_single(self, input):
+class RiskModel(BaseEstimator):
+    """ A template estimator to be used as a reference implementation.
+    For more information regarding how to build your own estimator, read more
+    in the :ref:`User Guide <user_guide>`.
+    Parameters
+    ----------
+    demo_param : str, default='demo_param'
+        A parameter used for demonstation of how to pass and store paramters.
+    Examples
+    --------
+    >>> from skltemplate import TemplateEstimator
+    >>> import numpy as np
+    >>> X = np.arange(100).reshape(100, 1)
+    >>> y = np.zeros((100, ))
+    >>> estimator = TemplateEstimator()
+    >>> estimator.fit(X, y)
+    TemplateEstimator(demo_param='demo_param')
+    """
+    def __init__(self, sample_weights_csv_file = None, data_headers = None, fold_csv_file = None, params = None, settings = None, show_omitted_variables = False):
+        self.sample_weights_csv_file = sample_weights_csv_file
+        self.data_headers = data_headers
+        self.fold_csv_file = fold_csv_file
+        self.settings = settings
+        self.show_omitted_variables = show_omitted_variables
+        self.threshold = 0.5
 
-    	input = np.array(input)[self.filter_mask]
-    	score = float(np.dot(input, self.rho_values.T))
-    	return 1.0/(1.0 + math.exp(-(self.intercept_val + score)))
+        self.max_coefficient = params['max_coefficient']
+        self.max_L0_value = params['max_L0_value']
+        self.max_offset = params['max_offset']
+        self.c0_value = params['c0_value']
+        self.w_pos = params['w_pos']
 
-    def test_model(self, test_data):
+    def fit_transform(self, X, y):
+        """A reference implementation of a fitting function.
+        Parameters
+        ----------
+        X : {array-like, sparse matrix}, shape (n_samples, n_features)
+            The training input samples.
+        y : array-like, shape (n_samples,) or (n_samples, n_outputs)
+            The target values (class labels in classification, real numbers in
+            regression).
+        Returns
+        -------
+        self : object
+            Returns self.
+        """
+        X, y = check_X_y(X, y, accept_sparse=True)
+        self.is_fitted_ = True
+        # `fit` should always return `self`
 
-    	test_data['X'] = test_data['X'][:,1:]
-    	test_data['X'] = test_data['X'][:,self.filter_mask]
+        # transforming data
+        raw_data = np.insert(X, 0, y, axis=1)
+        N = raw_data.shape[0]
 
-    	scores = (np.squeeze(np.asarray(np.dot(test_data['X'], self.rho_values.T)))).astype('float64')
-    	for i,score in enumerate(scores):
-    		scores[i] = float(1.0/(1.0 + math.exp(-(self.intercept_val + score))))
-    	# print(scores)
+        # setup Y vector and Y_name
+        Y_col_idx = [0]
+        Y = raw_data[:, Y_col_idx]
+        Y_name = self.data_headers[Y_col_idx[0]]
+        Y[Y == 0] = -1
 
-    	n = 0
-    	N = len(scores)
+        # setup X and X_names
+        X_col_idx = [j for j in range(raw_data.shape[1]) if j not in Y_col_idx]
+        X = raw_data[:, X_col_idx]
+        variable_names = [self.data_headers[j] for j in X_col_idx]
 
-    	# testing on test data
-    	for i,x in enumerate(test_data['Y']):
-    		y_value = x[0]
+        # insert a column of ones to X for the intercept
+        X = np.insert(arr=X, obj=0, values=np.ones(N), axis=1)
+        variable_names.insert(0, '(Intercept)')
 
-    		if y_value == -1:
-    			y_value = 0
+        if self.sample_weights_csv_file is None:
+            sample_weights = np.ones(N)
+        else:
+            if os.path.isfile(self.sample_weights_csv_file):
+                sample_weights = pd.read_csv(self.sample_weights_csv_file, sep=',', header=None)
+                sample_weights = sample_weights.as_matrix()
+            else:
+                raise IOError('could not find sample_weights_csv_file: %s' % self.sample_weights_csv_file)
 
-    		if y_value == round(scores[i] - self.threshold + 0.5):
-    			n += 1
+        self.data = {
+            'X': X,
+            'Y': Y,
+            'variable_names': variable_names,
+            'outcome_name': Y_name,
+            'sample_weights': sample_weights,
+            }
 
-    	print(n)
-    	print(N)
-    	print(n/N)
-    	print(scores[0:20])
-    	print("_____________")
+        #load folds
+        if self.fold_csv_file is not None:
+            if not os.path.isfile(self.fold_csv_file):
+                raise IOError('could not find fold_csv_file: %s' % self.fold_csv_file)
+            else:
+                fold_idx = pd.read_csv(self.fold_csv_file, sep=',', header=None)
+                fold_idx = fold_idx.values.flatten()
+                K = max(fold_idx)
+                all_fold_nums = np.sort(np.unique(fold_idx))
+                assert len(fold_idx) == N, "dimension mismatch: read %r fold indices (expected N = %r)" % (len(fold_idx), N)
+                assert np.all(all_fold_nums == np.arange(1, K+1)), "folds should contain indices between 1 to %r" % K
+                assert fold_num in np.arange(0, K+1), "fold_num should either be 0 or an integer between 1 to %r" % K
+                if fold_num >= 1:
+                    test_idx = fold_num == fold_idx
+                    train_idx = fold_num != fold_idx
+                    data['X'] = data['X'][train_idx,]
+                    data['Y'] = data['Y'][train_idx]
+                    data['sample_weights'] = data['sample_weights'][train_idx]
 
+        assert check_data(self.data)
+        
+        # create coefficient set and set the value of the offset parameter
+        coef_set = CoefficientSet(variable_names = self.data['variable_names'], lb = -self.max_coefficient, ub = self.max_coefficient, sign = 0)
+        conservative_offset = get_conservative_offset(self.data, coef_set, self.max_L0_value)
+        self.max_offset = min(self.max_offset, conservative_offset)
+        coef_set['(Intercept)'].ub = self.max_offset
+        coef_set['(Intercept)'].lb = -self.max_offset
+
+        constraints = {
+            'L0_min': 0,
+            'L0_max': self.max_L0_value,
+            'coef_set':coef_set,
+        }
+
+        # fit using ltca
+        model_info, mip_info, lcpa_info = run_lattice_cpa(self.data, constraints, self.settings)
+        # print_model(model_info['solution'], data)
+        rho = model_info['solution']
+        print_model(model_info['solution'], self.data)
+
+        # fitting model
+        variable_names = self.data['variable_names']
+        rho_values = np.copy(rho)
+        rho_names = list(variable_names)
+
+        print(rho_values)
+
+        print("rho values %d" % len(rho_values))
+
+        # removes intercept value or sets it to 0
+        if '(Intercept)' in rho_names:
+            intercept_ind = variable_names.index('(Intercept)')
+            self.intercept_val = int(rho[intercept_ind])
+            rho_values = np.delete(rho_values, intercept_ind)
+            rho_names.remove('(Intercept)')
+        else:
+            self.intercept_val = 0
+
+        self.filter_mask = np.array(rho_values) != 0
+
+        # removes zero values
+        if not self.show_omitted_variables:
+            selected_ind = np.flatnonzero(rho_values)
+            self.rho_values = rho_values[selected_ind]
+            self.rho_names = [rho_names[i] for i in selected_ind]
+
+        print(self.rho_values)
+        print(self.filter_mask)
+
+        #rm = RiskModel(rho_values, rho_names, intercept_val, selected_ind, filter_mask != 0)
+        return self
+
+    def predict(self, X):
+        """ A reference implementation of a predicting function.
+        Parameters
+        ----------
+        X : {array-like, sparse matrix}, shape (n_samples, n_features)
+            The training input samples.
+        Returns
+        -------
+        y : ndarray, shape (n_samples,)
+            Returns an array of ones.
+        """
+        X = check_array(X, accept_sparse=True)
+        X = X[:,self.filter_mask]
+
+        scores = (np.squeeze(np.asarray(np.dot(X, self.rho_values.T)))).astype('float64')
+        y = np.array(scores)
+
+        for i,score in enumerate(scores):
+            y[i] = round(float(1.0/(1.0 + math.exp(-(self.intercept_val + score)))) - self.threshold + 0.5)
+
+        #check_is_fitted(self, 'is_fitted_')
+        return y
 
     		
     		
